9b73b7730b0fa19c7467afc50830d68220939abc
pighouse7647 <pighouse7647@gmail.com>
2018-08-12 10:37:08 +0800

    1.add basic process control

---
+++ lecture-w1-java/src/ProcessControlOne/ProcessControl.java
@@ -0,0 +1,33 @@
+package ProcessControlOne;
+
+public class ProcessControl {
+
+	public static void main(String[] args) {
+		mothod1();
+
+	}
+
+	private static void mothod1() {
+		int b = 10;
+		int a = 11;
+		if(a > b) {
+			System.out.println(a + " > " + b);
+		} else if(a < b){
+			System.out.println(a + " < " + b);
+		} else {
+			System.out.println(a + " = " + b);
+		}
+	}
+	private static void mothod1_1() {
+		int b = 10;
+		int a = 11;
+		if(a > b) {
+			System.out.println(a + " > " + b);
+		} else if(a < b){
+			System.out.println(a + " < " + b);
+		} else {
+			System.out.println(a + " = " + b);
+		}
+	}
+
+}

3212ad21f44cc493dbba5d6403d4d84da1ca5e5b
pighouse7647 <pighouse7647@gmail.com>
2018-08-12 10:53:06 +0800

    1.add while control.

--- lecture-w1-java/src/ProcessControlOne/ProcessControl.java
+++ lecture-w1-java/src/ProcessControlOne/ProcessControl.java
@@ -3,11 +3,12 @@ package ProcessControlOne;
 public class ProcessControl {
 
 	public static void main(String[] args) {
-		mothod1();
-
+//		method1();
+//		method2((int) (Math.random()*10), (int) (Math.random()*10));
+		method3();
 	}
 
-	private static void mothod1() {
+	private static void method1() {
 		int b = 10;
 		int a = 11;
 		if(a > b) {
@@ -18,15 +19,21 @@ public class ProcessControl {
 			System.out.println(a + " = " + b);
 		}
 	}
-	private static void mothod1_1() {
-		int b = 10;
-		int a = 11;
+	private static void method2(int a, int b) {
 		if(a > b) {
 			System.out.println(a + " > " + b);
 		} else if(a < b){
 			System.out.println(a + " < " + b);
 		} else {
 			System.out.println(a + " = " + b);
+		}
+	}
+	private static void method3() {
+		int i = 0;
+		while (i < 100) {
+			System.out.printf("%3d: ", i + 1); // align at 3 digits
+			method2((int) (Math.random()*10), (int) (Math.random()*10));
+			i++;
 		}
 	}
 
